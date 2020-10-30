6273541af6605c98ad119b6a38be1154c68e81d2
estrattonbailey <ericstrattonbailey@gmail.com>
2020-10-30 14:31:55 -0500

    update parse-ssn, improve SSNInput

--- packages/forms/src/SSNInput.tsx
+++ packages/forms/src/SSNInput.tsx
@@ -13,42 +13,61 @@ import { Label } from './Label';
 
 export type SSNProps = {
   onUpdate?(ssn: string): void;
+  masker?: string;
+  separator?: string;
 };
-export type SSNInputProps = InputProps & SSNProps & Partial<Pick<FieldInputProps<string>, 'value'>>;
+export type SSNInputProps = InputProps &
+  SSNProps &
+  Partial<Pick<FieldInputProps<string>, 'value'>>;
 export type SSNInputFieldProps = InputFieldProps & SSNProps;
 export type SSNInputFieldWithLabelProps = InputFieldWithLabelProps & SSNProps;
 
 export function SSNInput(props: SSNInputProps) {
-  const [raw, setRaw] = React.useState('');
-  const [formatted, setFormatted] = React.useState(format(mask(props.value || '')));
+  const { value = '', masker = '*', separator = '-' } = props;
+
+  // if initial value is masked, reset to empty string
+  const initialValue = new RegExp(`[^0-9${separator}]`).test(value)
+    ? ''
+    : clean(value, masker);
+  const formattedInitialValue = format(mask(initialValue, masker), separator);
+
+  const [raw, setRaw] = React.useState(initialValue);
+  const [formatted, setFormatted] = React.useState(formattedInitialValue);
 
   const onChange = React.useCallback(
     (e) => {
-      e.persist()
+      e.persist();
 
-      const value = e.target.value
+      const inputValue = e.target.value;
+      const nextChar = inputValue.slice(-1);
 
-      // if a user types a letter, or the last character is a formatting
-      // character, just ignore
-      if (/[^0-9-*]/.test(value.slice(-1))) return
+      // if a user types a letter, the last character is a formatting
+      // character, or we already have a full SSN, just ignore
+      if (
+        new RegExp(`[^0-9${separator}${masker}]`).test(nextChar) ||
+        inputValue.length > 11
+      )
+        return;
 
-      const cleanedInputValue = clean(value, '*'); // masked, no - format
+      const cleanedInputValue = clean(inputValue, masker); // masked, no - format
       const rawSSN = clean(raw); // no formatting, just numbers
 
       // if user deleted a character, remove, otherwise, append
       const next =
         cleanedInputValue.length < rawSSN.length
           ? rawSSN.slice(0, cleanedInputValue.length)
+          : new RegExp(`[${separator}${masker}]`).test(nextChar)
+          ? rawSSN
           : rawSSN + cleanedInputValue.slice(-1);
 
-      setRaw(format(next));
-      setFormatted(format(mask(next)));
+      setRaw(next);
+      setFormatted(format(mask(next, masker), separator));
     },
     [raw, setRaw, formatted, setFormatted]
   );
 
   const resetCursor = React.useCallback((e) => {
-    e.persist()
+    e.persist();
 
     const len = e.target.value.length;
 
@@ -84,6 +103,7 @@ export function SSNInputField({ name, validate, ...rest }: SSNInputFieldProps) {
             {...field}
             hasError={hasError}
             onUpdate={(ssn) => {
+              console.log(ssn)
               form.setFieldValue(name, ssn);
             }}
           />
