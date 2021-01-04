b6111524fd0a1e4c354175f09b724e6e9dd1348f
estrattonbailey <ericstrattonbailey@gmail.com>
2021-01-04 13:17:47 -0600

    break out monorepo

--- packages/forms/src/SubGroup.tsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-import { Box, BoxProps } from '@truework/ui';
-
-export const SubGroup = styled(Box)<BoxProps>(
-  ({ theme }) => `
-    border-left: 2px solid currentColor;
-    padding-left: ${theme.space.sm};
-  `
-);
